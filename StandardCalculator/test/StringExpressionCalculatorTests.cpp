8a46d0746a76facf724cc0c1125d89531eeeb0bd
Slawomir Grabowski <grabowski@quickturnstudio.com>
2021-11-13 11:00:17 +0100

    save

--- StandardCalculator/test/StringExpressionCalculatorTests.cpp
+++ StandardCalculator/test/StringExpressionCalculatorTests.cpp
@@ -1,9 +1,3 @@
-/*
- * Copyright (C) Quick Turn Studio GmbH - All Rights Reserved
- * Unauthorized copying of this file, via any medium is strictly prohibited
- * Proprietary and confidential
- */
-
 #include <catch2/catch.hpp>
 
 #include <StringExpressionCalculator.h>
